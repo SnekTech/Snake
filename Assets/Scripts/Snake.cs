ad3aef41b98b2d0155059bb1c0af3e76ad9b2059
syf <2654525303@qq.com>
2021-02-05 16:09:24 +0800

    use unity input system package

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private PlayerControls _playerControls;
+    
+    [SerializeField] private float force = 10;
+    [SerializeField] private float initialForce = 8;
+    [SerializeField] private float k = 5;
+    [SerializeField] private Rigidbody head;
+
+    private void Awake()
+    {
+        _playerControls = new PlayerControls();
+    }
+
+    private void OnEnable()
+    {
+        _playerControls.Enable();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        head.AddForce(GetForwardDirection() * force);
+    }
+
+    private void OnDisable()
+    {
+        _playerControls.Disable();
+    }
+
+    private bool IsMoving()
+    {
+        return head.velocity != Vector3.zero;
+    }
+
+    private Vector3 GetForwardDirection()
+    {
+        // push the snake forward, use USER INPUT later
+        return Vector3.forward;
+    }
+}
